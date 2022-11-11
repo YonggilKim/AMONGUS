d2c893d92185a9f644dd446ac739901c9e623b2d
yonggil kim <gtmddls41@gmail.com>
2022-11-12 00:21:27 +0900

    메인메뉴UI
    메인메뉴  UI
    URP 세팅

---
+++ AMONGUS/Assets/Script/SettingsUI.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsUI : MonoBehaviour
+{
+    [SerializeField] private Button MounseCtrlButton;
+    [SerializeField] private Button keyboardMounseCtrlButton;
+    private Animator animator;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void OnEnable()
+    {
+        switch (PlayerSettings.ctrlType)
+        {
+            case ECtrlType.Mouse:
+                MounseCtrlButton.image.color = Color.green;
+                keyboardMounseCtrlButton.image.color = Color.white;
+                break;
+            case ECtrlType.keyboardMouse:
+
+                MounseCtrlButton.image.color = Color.white;
+                keyboardMounseCtrlButton.image.color = Color.green;
+                break;
+        }
+    }
+    public void SetControlMode(int ctrlType)
+    {
+        PlayerSettings.ctrlType = (ECtrlType)ctrlType;
+        switch (PlayerSettings.ctrlType)
+        {
+            case ECtrlType.Mouse:
+                MounseCtrlButton.image.color = Color.green;
+                keyboardMounseCtrlButton.image.color = Color.white;
+                break;
+            case ECtrlType.keyboardMouse:
+
+                MounseCtrlButton.image.color = Color.white;
+                keyboardMounseCtrlButton.image.color = Color.green;
+                break;
+        }
+    }    
+    public void Close()
+    {
+        StartCoroutine(CloseAfterDelay());
+    }
+    private IEnumerator CloseAfterDelay()
+    {
+        animator.SetTrigger("close");
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+        animator.ResetTrigger("close");
+    }
+}
