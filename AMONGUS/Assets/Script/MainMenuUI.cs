ed30e4b656e4b181002772a0328c94f499fcfffa
yonggil kim <gtmddls41@gmail.com>
2022-11-12 23:57:04 +0900

    Change Rider
    1

--- AMONGUS/Assets/Script/MainMenuUI.cs
+++ AMONGUS/Assets/Script/MainMenuUI.cs
@@ -6,7 +6,7 @@ public class MainMenuUI : MonoBehaviour
 {
     public void OnClickOnlineButton()
     {
-        Debug.Log("Click Online");
+        //Debug.Log("Click Online");
     }
     public void OnClickQuitButton()
     {
